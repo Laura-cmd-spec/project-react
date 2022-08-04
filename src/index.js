5ba5c556e4d8e79af915b84fc99f8f85269d66c6
Laura Krupa <laura.krupa@astellas.com>
2022-08-04 20:53:53 +0200

    start

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import './styles/normalize.scss';
+import './styles/global.scss';
+import ReactDOM from 'react-dom';
+import App from './App';
+
+ReactDOM.render(
+  <App />,
+  document.querySelector('#root')
+);
